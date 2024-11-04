b69001961d0276b179a7dd4088bf313d768acf85
Tomek <polskirick2008@gmail.com>
2024-11-04 17:04:28 +0100

    Update fxmanifest.lua
    Rebranding

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,12 +1,15 @@
 fx_version 'cerulean'
 game 'gta5'
 
-author 'polskirick'
+author '_Mequ_'
 description 'Skrypt na taniec z gta online'
 
 client_scripts {
     'config.lua',
     'client.lua',
+}
+server_scripts {
+    'server.lua'
 }
 
 lua54 'yes'
