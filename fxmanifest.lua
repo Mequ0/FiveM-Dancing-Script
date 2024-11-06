cd42b74c7d9498d9246d31313d53098374646626
Tomek <88977612+Mequ0@users.noreply.github.com>
2024-11-06 16:49:41 +0100

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,7 +2,7 @@ fx_version 'cerulean'
 game 'gta5'
 
 author '_Mequ_'
-description 'Skrypt na taniec z gta online'
+description 'Skrypt na taniec z gta online https://github.com/Mequ0/FiveM-Dancing-Script'
 
 client_scripts {
     'config.lua',
